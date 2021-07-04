f4edf22b5056b5da4f0d79cea165d72218996365
ASUS <alitaghipour80@gmail.com>
2021-07-04 20:13:34 +0430

    Unversioned files

---
+++ src/Java/ScoreBoardPlayer.java
@@ -0,0 +1,11 @@
+public class ScoreBoardPlayer {
+    private int rank;
+    private int score;
+    private String nickname;
+
+    public ScoreBoardPlayer(int rank, int score, String nickname) {
+        this.rank = rank;
+        this.score = score;
+        this.nickname = nickname;
+    }
+}
