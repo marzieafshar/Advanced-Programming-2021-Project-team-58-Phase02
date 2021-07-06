654e21f61809338c67dafd37490729ea0a93d4c7
ASUS <alitaghipour80@gmail.com>
2021-07-06 21:37:46 +0430

    profileMenu started

--- src/main/java/Controller/ProfileController.java
+++ src/main/java/Controller/ProfileController.java
@@ -1,17 +1,23 @@
 package Controller;
 
+import Model.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -27,10 +33,35 @@ public class ProfileController implements Initializable {
     @FXML
     private TextField nicknameChangeField;
     @FXML
-    private Circle myCircle;
+    private ImageView itemImage;
+    @FXML
+    private Label username;
+    @FXML
+    private Label userScore;
+    @FXML
+    private Label userMoney;
+    @FXML
+    private Label userNumOfDecks;
+    @FXML
+    private Label userActiveDeckName;
+    @FXML
+    private Label userNumOfWins;
+    @FXML
+    private Label userNumOfLosses;
+    @FXML
+    private GridPane gridPane;
     private Stage stage;
+    private MyListener myListener;
 
-    public void nicknameChange(ActionEvent event) {
+    @FXML
+    void backToMainMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/MainMenu.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+
+    @FXML
+    void nicknameChange(javafx.scene.input.MouseEvent event) {
         String newNickName = nicknameChangeField.getText();
         if (nicknameChangeField == null) return;
         Controller.getLoggedInPlayer().setNickname(newNickName);
@@ -38,7 +69,9 @@ public class ProfileController implements Initializable {
         alert.setContentText("Nickname Changed Successfully !");
         alert.showAndWait();
     }
-    public void passwordChange(ActionEvent event) {
+
+    @FXML
+    public void passwordChange(javafx.scene.input.MouseEvent event) {
         String newPassword1 = passwordChangeField.getText();
         String newPassword2 = repeatPasswordChangeField.getText();
         if (newPassword2 == null || newPassword1 == null) return;
@@ -53,16 +86,74 @@ public class ProfileController implements Initializable {
             alert.showAndWait();
         }
     }
-    public void BackToMainMenu(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/MainMenu.fxml")));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Image image = new Image(getClass().getResourceAsStream("/Images/Monster/Bitron.jpg"));
-        myCircle.setFill(new ImagePattern(image));
+        setUserInfo();
+        myListener = new MyListener() {
+            @Override
+            public void onClickListener(Object object) {
+                setPlayerImage((Image) object);
+            }
+        };
+        showAllAvatars();
+    }
 
+    private void showAllAvatars() {
+        int column = 0;
+        int row = 0;
+        for (int i = 0; i < 35; i++) {
+            try {
+                gridPane.setMinHeight(Region.USE_COMPUTED_SIZE);
+                gridPane.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                gridPane.setMaxHeight(Region.USE_PREF_SIZE);
+
+                gridPane.setMinWidth(Region.USE_COMPUTED_SIZE);
+                gridPane.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                gridPane.setMaxWidth(Region.USE_PREF_SIZE);
+
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/Avatars.fxml"));
+                AnchorPane anchorPane = fxmlLoader.load();
+
+                String str = "/Images/Characters/Chara001.dds" + i + ".png";
+                AvatarsController avatarsController = fxmlLoader.getController();
+                avatarsController.setImage( new Image(getClass().getResourceAsStream(str)), myListener);
+                if (column == 7) {
+                    column = 0;
+                    row++;
+                }
+
+                gridPane.add(anchorPane , column++ , row);
+
+                GridPane.setMargin(anchorPane, new Insets(2));
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setPlayerImage(Image image) {
+        Player player = Controller.getLoggedInPlayer();
+        player.setImage(image);
+    }
+
+    private void setUserInfo() {
+        Player player = Controller.getLoggedInPlayer();
+        itemImage.setImage(player.getImage());
+        username.setText(player.getUsername());
+        userMoney.setText(String.valueOf(player.getMoney()));
+        userScore.setText(String.valueOf(player.getScore()));
+        if (player.getActiveDeck() == null)
+            userActiveDeckName.setText("Not set yet");
+        else
+        userActiveDeckName.setText(player.getActiveDeck().getDeckName());
+        userNumOfDecks.setText(String.valueOf(player.getDecks().size()));
+        userNumOfWins.setText(String.valueOf(player.getWinMatches()));
+        userNumOfLosses.setText(String.valueOf(player.getLoseMatches()));
+        nicknameChangeField.setPromptText("Your nickname: "+player.getNickname());
     }
 }
+
